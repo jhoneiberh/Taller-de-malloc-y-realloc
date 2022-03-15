2771baf56034b548b74c1774d6cd0aaf95953fc5
jhoneiberh <jhoneiberh@gmail.com>
2022-03-15 16:25:10 -0500

    Version definitiva del proyecto, todas las funcionalidades hechas

--- mem_alloc_lab1.c
+++ mem_alloc_lab1.c
@@ -1,30 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include<wchar.h>
+#include <wchar.h>
 
 #define RESERVAR_MAS 1
+#define TAM_INICIAL 5
+
+void solo_realloc();
+void realloc_y_memset();
 
 int main()
 {
    //declaración de un el arreglo dinámico de enteros:
    int *edades;
    
-   int tam_inicial = 5; //tamaño inicial
-   int incremento = 0;
-
-   
-   //incremento de memoria (entero):
-   
+    //tamaño inicial
+   int incremento = 0;   
 
    //reserve un espacio en memoria inicial para 5 enteros:
-   edades = (int *) malloc(tam_inicial * sizeof(int));
+   edades = (int *) malloc(TAM_INICIAL * sizeof(int));
      
    int sel = RESERVAR_MAS;
-   int aumento_reserva = 0;
 
-   /**
-   Codigo completo
-   **/
    if(edades == NULL)
    {
       printf("Memoria no asignada\n");
@@ -34,73 +30,99 @@ int main()
    {
       printf("\tAsignacion de memoria exitosa\n");
 
-      // Asignacion base de 5 elementos
-      wmemset(edades, 10, tam_inicial);
-      /* for(int i = 0; i < tam_inicial; i++)
-      {
-         edades[i] = i * 2;
-      } */
-
-
-      // Imprimiendo valores del arreglo
-      printf("\tLos numeros del arreglo son: \n");
-      for(int i = 0; i < tam_inicial; i++)
-      {
-         printf("El numero: %i es %i\n", i, edades[i]);
-      }
-
-
       /*
-      Reasignacion de memoria
+         Escoger procedimiento a llevar a cabo
       */
-      do
-      {
-         printf("\nDesea reservar más espacio?");
-         scanf("%i",&sel);
-
-
-         if(sel == RESERVAR_MAS)
-         {
-            printf("\nCuantos elementos desea agregar?");
-            scanf("%i", &incremento);
-         }
-         else
-         {
-            exit(0);
-         }
-
-         // Reasignacion de memoria         
-         edades = realloc(edades, incremento * sizeof(int));
-         
-
-         if(edades == NULL)
-         {
-            printf("\tNo se realizó la reasignacion de memoria\n");
-            exit(0);
-         }
-         else
-         {
-            printf("\tReasignacion de memoria exitosa\n");
-
-            // Asignacion de valores
-            wmemset(edades, 1, incremento);
-            /* for(int i = 0; i < incremento; i++)
-            {
-               edades[i] = i * 2;
-            } */
-
-            // Imprimiendo valores
-            printf("\tLos nuevos numeros del arreglo son: \n");
-            for(int i = 0; i < incremento; i++)
-            {
-               printf("El numero %i es: %i\n", i, edades[i]);
-            }
-
-         }
-
-      }while(sel == RESERVAR_MAS);
+      //solo_realloc();
+      realloc_y_memset();
    }
 
    return 0;
+}
+
+/*
+   Funcion que solo se encarga de aumentar o disminuir memoria sin rellenar datos
+*/
+void solo_realloc(int sel, int incremento, int *edades)
+{
+   do
+   {
+      printf("\nDesea reservar más espacio?");
+      scanf("%i",&sel);
+
+
+      if(sel == RESERVAR_MAS)
+      {
+         printf("\nCuantos elementos desea agregar?");
+         scanf("%i", &incremento);
+      }
+      else
+      {
+         exit(0);
+      }
+
+      // Reasignacion de memoria         
+      edades = realloc(edades, incremento * sizeof(int));
+      
+      if(edades == NULL)
+      {
+         printf("\tNo se realizó la reasignacion de memoria\n");
+         exit(0);
+      }
+      else
+      {
+         printf("\tReasignacion de memoria exitosa\n");
+      }
+
+   }while(sel == RESERVAR_MAS);
+}
+
+/*
+   Funcion que permite aumentar la memoria ademas de rellenar el arreglo con datos usando memset
+*/
+void realloc_y_memset(int sel, int incremento, int *edades)
+{
+   do
+   {
+      printf("\nDesea reservar más espacio?");
+      scanf("%i",&sel);
+
+
+      if(sel == RESERVAR_MAS)
+      {
+         printf("\nCuantos elementos desea agregar?");
+         scanf("%i", &incremento);
+      }
+      else
+      {
+         exit(0);
+      }
+
+      // Reasignacion de memoria         
+      edades = realloc(edades, incremento * sizeof(int));
+      
+
+      if(edades == NULL)
+      {
+         printf("\tNo se realizó la reasignacion de memoria\n");
+         exit(0);
+      }
+      else
+      {
+         printf("\tReasignacion de memoria exitosa\n");
+
+         // Asignacion de valores
+         wmemset(edades, 1, incremento);
+
+         // Imprimiendo valores
+         printf("\tLos nuevos numeros del arreglo son: \n");
+         for(int i = 0; i < incremento; i++)
+         {
+            printf("El numero %i es: %i\n", i, edades[i]);
+         }
+
+      }
+
+   }while(sel == RESERVAR_MAS);
 
 }
